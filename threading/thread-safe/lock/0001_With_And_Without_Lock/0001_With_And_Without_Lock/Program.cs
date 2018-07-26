6746e3bac4f7ff8b5fbc4bdec8c883d42b3ef7d9
Pruthvi Raju <pruthvi.raju@live.com>
2018-07-26 22:05:09 +0530

    With and without lock program

---
+++ threading/thread-safe/lock/0001_With_And_Without_Lock/0001_With_And_Without_Lock/Program.cs
@@ -0,0 +1,63 @@
+﻿///----------------------------------------------------------------------
+/// <File>Program</File>
+/// <Author>Pruthvi</Author>
+/// <Date>July 26th, 2018</Date>
+/// <Summary></Summary>
+///-----------------------------------------------------------------------
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0001_With_And_Without_Lock
+{
+    class Program
+    {
+        private static readonly object obj = new object();
+        static void Main(string[] args)
+        {
+            int counter_with_lock = 0;
+
+            int counter_without_lock = 0;
+
+            Task t1 = new Task(() =>
+                {
+                    for (int i = 0; i < 10000000; i++)
+                    {
+                        counter_without_lock++;
+                        lock (obj)
+                        {
+                            counter_with_lock++;
+                        }
+                    }
+                }
+            );
+
+            Task t2 = new Task(() =>
+                {
+                    for (int i = 0; i < 10000000; i++)
+                    {
+                        counter_without_lock++;
+                        lock (obj)
+                        {
+                            counter_with_lock++;
+                        }
+                    }
+                }
+            );
+
+            t1.Start();
+            t2.Start();
+
+            t1.Wait();
+            t2.Wait();
+
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"We expects result as 20000000 for value types.");
+            builder.AppendLine($"Actual : ");
+            builder.AppendLine($"counter_without_lock - {counter_without_lock} (this may vary on every run, but still inconsistent)");
+            builder.AppendLine($"counter_with_lock - {counter_with_lock}");
+            Console.WriteLine(builder.ToString());
+            Console.ReadLine();
+        }
+    }
+}
