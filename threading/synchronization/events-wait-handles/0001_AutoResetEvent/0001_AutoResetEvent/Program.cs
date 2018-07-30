1e204db2f3ccf13c080c98b8e64d9d2c5ae3e78a
Pruthvi Raju <pruthvi.raju@live.com>
2018-07-30 22:54:54 +0530

    0001_AutoResetEvent example for signaling.

---
+++ threading/synchronization/events-wait-handles/0001_AutoResetEvent/0001_AutoResetEvent/Program.cs
@@ -0,0 +1,42 @@
+﻿///----------------------------------------------------------------------
+/// <File>Program</File>
+/// <Author>Pruthvi</Author>
+/// <Date>July 30th, 2018</Date>
+/// <Summary></Summary>
+///-----------------------------------------------------------------------
+
+namespace _0001_AutoResetEvent
+{
+    using System;
+    using System.Threading;
+    class Program
+    {
+        static AutoResetEvent autoEvent;
+
+        static void DoWork()
+        {
+            Console.WriteLine("   worker thread started, now waiting on event...");
+            autoEvent.WaitOne();
+            Console.WriteLine("   worker thread reactivated, now exiting... press enter");
+        }
+
+        static void Main()
+        {
+            autoEvent = new AutoResetEvent(false);
+
+            Console.WriteLine(" main thread starting worker thread...");
+            Thread t = new Thread(DoWork);
+            t.Start();
+
+            Console.WriteLine(" main thread sleeping for 5 second...");
+            Thread.Sleep(5000);
+            Console.WriteLine(" main thread signaling worker thread... press enter..");
+
+            Console.ReadLine();
+
+            autoEvent.Set();
+
+            Console.ReadLine();
+        }
+    }
+}
