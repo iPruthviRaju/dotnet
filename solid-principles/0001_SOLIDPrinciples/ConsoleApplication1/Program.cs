0a12816384e8bc180ebc3f74aad5676cf4d36883
Pruthvi Raju <pruthvi.raju@live.com>
2018-08-04 23:53:10 +0530

    OCP minors

---
+++ solid-principles/0001_SOLIDPrinciples/ConsoleApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿///----------------------------------------------------------------------
+/// <File>Employee</File>
+/// <Author>Pruthvi</Author>
+/// <Date>August 4th, 2018</Date>
+/// <Summary></Summary>
+///-----------------------------------------------------------------------
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ solid-principles/0001_SOLIDPrinciples/OCP/Employee.cs
@@ -0,0 +1,27 @@
+﻿///----------------------------------------------------------------------
+/// <File>Employee</File>
+/// <Author>Pruthvi</Author>
+/// <Date>August 4th, 2018</Date>
+/// <Summary></Summary>
+///-----------------------------------------------------------------------
+
+namespace OCP
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Employee() { }
+        public Employee(int id, string name)
+        { this.ID = id; this.Name = name; }
+        public decimal CalculateBonus(decimal salary)
+        {
+            return salary * .1M;
+        }
+    }
+}
