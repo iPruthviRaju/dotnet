bd6555ab66119a4e12e20fcf0b351f87cfea02de
Pruthvi Raju <pruthvi.raju@live.com>
2018-08-05 19:20:25 +0530

    TTD sample project

---
+++ ttd/0001_UnitTest/0001_UnitTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿///----------------------------------------------------------------------
+/// <File>UnitTest1</File>
+/// <Author>Pruthvi</Author>
+/// <Date>August 5th, 2018</Date>
+/// <Summary></Summary>
+///-----------------------------------------------------------------------
+
+namespace _0001_UnitTest
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ClassLibrary1;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            MathsComponents mathsComponents = new MathsComponents();
+            int result = mathsComponents.Add(10, 10);
+            Assert.AreEqual(20, result);
+        }
+
+        [TestMethod]
+        public void TestSubstract()
+        {
+            MathsComponents mathsComponents = new MathsComponents();
+            int result = mathsComponents.Substract(20, 10);
+            Assert.AreEqual(10, result);
+        }
+    }
+}
---
+++ ttd/0001_UnitTest/ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿///----------------------------------------------------------------------
+/// <File>ClassLibrary1</File>
+/// <Author>Pruthvi</Author>
+/// <Date>August 5th, 2018</Date>
+/// <Summary></Summary>
+///-----------------------------------------------------------------------
+
+namespace ClassLibrary1
+{
+    public class MathsComponents
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Substract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
