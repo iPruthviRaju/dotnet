f6527ee95923d1dfe36223608d3b7a65892aaf14
Pruthvi Raju <pruthvi.raju@live.com>
2018-08-06 23:58:54 +0530

    Linq deferred vs immediate

---
+++ linq/DeferredVsImmediate/0001_Sample/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using _0001_Sample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student() { ID = 1, Name = "Pruthvi1"},
+                new Student() {ID = 2, Name = "Avinash1" }
+            };
+
+            var deferredStudents = from student in students select student;
+            var immediateStudens = students.ToList();
+
+            students.Add(new Student() { ID = 3, Name = "Pruthvi2"});
+            students.Add(new Student() { ID = 4, Name = "Avinash2" });
+        }
+    }
+}
