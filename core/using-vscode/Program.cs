7c2973b96edc2e063cf3ca11d3c378d5385d0c85
Pruthvi Raju <pruthvi.raju@live.com>
2018-09-02 23:39:01 +0530

    Dot net core - using vs code.

---
+++ core/using-vscode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace using_vscode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Reverse of string Pruthvi is {ReverseString("Pruthvi")}");
+        }
+
+        static string ReverseString(string input)
+        {
+            int length = input.Length;
+
+            char[] reverseChar = new char[length];
+
+            for(int incrementer = 0, decrementer = length - 1; incrementer < length; incrementer++, decrementer--)
+            {
+                reverseChar[incrementer] = input[decrementer];
+            }
+
+            return new string(reverseChar);
+        }
+    }
+}
